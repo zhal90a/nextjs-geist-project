cee4b5e7b36a3e0363ae6c3e95414ebb6430f5dc
zhal90a <anakmuda.kendari90@gmail.com>
2025-09-06 11:59:24 +0800

    Add prisma/migrations/20250905054515_add_administrator_model/migration.sql 1757131163572

---
+++ prisma/migrations/20250905054515_add_administrator_model/migration.sql
@@ -0,0 +1,14 @@
+-- CreateTable
+CREATE TABLE "Administrator" (
+    "id" INTEGER NOT NULL PRIMARY KEY AUTOINCREMENT,
+    "name" TEXT NOT NULL,
+    "email" TEXT NOT NULL,
+    "password" TEXT NOT NULL,
+    "role" TEXT NOT NULL DEFAULT 'admin',
+    "isActive" BOOLEAN NOT NULL DEFAULT true,
+    "createdAt" DATETIME NOT NULL DEFAULT CURRENT_TIMESTAMP,
+    "updatedAt" DATETIME NOT NULL
+);
+
+-- CreateIndex
+CREATE UNIQUE INDEX "Administrator_email_key" ON "Administrator"("email");
