a58eaf05134461140c0950883f917d55a6d0d712
zhal90a <anakmuda.kendari90@gmail.com>
2025-09-06 12:00:11 +0800

    Add .next/static/chunks/src_app_admin_dashboard_dosen_[id]_page_tsx_9148afbf._.js 1757131210487

---
+++ .next/static/chunks/src_app_admin_dashboard_dosen_[id]_page_tsx_9148afbf._.js
@@ -0,0 +1,7 @@
+(globalThis.TURBOPACK_CHUNK_LISTS = globalThis.TURBOPACK_CHUNK_LISTS || []).push({
+    script: typeof document === "object" ? document.currentScript : undefined,
+    chunks: [
+  "static/chunks/_946c3800._.js"
+],
+    source: "dynamic"
+});
